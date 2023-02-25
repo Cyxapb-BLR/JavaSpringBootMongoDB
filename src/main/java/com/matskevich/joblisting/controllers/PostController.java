acbeb2df56083426794d8aaaac8b98c060aea838
Cyxapb-BLR <cyxapb-511@yandex.ru>
2023-02-24 20:15:28 -0500

    created Post with @Document(collection = "JobPost"), PostRepository with ext MongoRepository, add getAllPosts(), http://localhost:8080/swagger-ui.html#/post-controller

--- src/main/java/com/matskevich/joblisting/controllers/PostController.java
+++ src/main/java/com/matskevich/joblisting/controllers/PostController.java
@@ -1,18 +1,35 @@
 package com.matskevich.joblisting.controllers;
 
+import com.matskevich.joblisting.models.Post;
+import com.matskevich.joblisting.repositories.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.annotations.ApiIgnore;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 public class PostController {
+
+    PostRepository postRepository;
+
+    @Autowired
+    public PostController(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
 
     @ApiIgnore
     @RequestMapping(value = "/")
     public void redirect(HttpServletResponse response) throws IOException {
         response.sendRedirect("/swagger-ui.html");
+    }
+
+    @GetMapping("/posts")
+    public List<Post> getAllPosts() {
+        return postRepository.findAll();
     }
 }
